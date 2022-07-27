1fecea7b0400abc94d8056ff245597be42e95231
Kyle Gach <kyle.gach@gmail.com>
2022-07-27 11:31:45 -0600

    Add `nextjs` and `nextjs_typescript` to repro configs

--- code/lib/cli/src/repro-templates.ts
+++ code/lib/cli/src/repro-templates.ts
@@ -21,6 +21,29 @@ const craTemplates = {
   },
 };
 
+const nextjsTemplates = {
+  'nextjs/default-js': {
+    name: 'Next.js (JavaScript)',
+    script: 'npx create-next-app .',
+    cadence: ['ci', 'daily', 'weekly'],
+    expected: {
+      framework: '@storybook/nextjs',
+      renderer: '@storybook/react',
+      builder: '@storybook/builder-webpack5',
+    },
+  },
+  'nextjs/default-ts': {
+    name: 'Next.js (TypeScript)',
+    script: 'npx create-next-app . --typescript',
+    cadence: ['ci', 'daily', 'weekly'],
+    expected: {
+      framework: '@storybook/nextjs',
+      renderer: '@storybook/react',
+      builder: '@storybook/builder-webpack5',
+    },
+  },
+};
+
 const reactViteTemplates = {
   'react-vite/default-js': {
     name: 'React Vite (JS)',
@@ -160,6 +183,7 @@ const vueCliTemplates = {
 
 export default {
   ...craTemplates,
+  ...nextjsTemplates,
   ...reactViteTemplates,
   ...vue2ViteTemplates,
   ...vue3ViteTemplates,
